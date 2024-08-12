c64e4e32b0785ee1ded14e7b98672cb6c8ecfd86
Tayfun <114905802+Falconul@users.noreply.github.com>
2024-08-12 10:15:49 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',  // GitHub Pages'de projeye özgü alt dizin yolu
   plugins: [react()],
-  base: '/', // Proje sayfası için
 });
